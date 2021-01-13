2c403d681256681165815125806b52f995c4cea0
Cameron Fischer <fischer.cameron@gmail.com>
2021-01-13 01:21:12 -0500

    cache always calls onSave; it's a promise

--- plugins/uglify/cache.js
+++ plugins/uglify/cache.js
@@ -12,15 +12,28 @@ getCacheForTiddler, except  that its cache is in a file, not in memory.
 /*global $tw: false */
 'use strict';
 
-var path = require('path');
-
+/**
+ * @param onSave is an async callback that takes (err, saved, value)
+ *    err - an error or if there was an error saving the cache
+ *    saved - boolean, true if the file cache was refreshed, false otherwise
+ *    value - the results of method.
+ */
 exports.getFileCacheForTiddler = function(wiki, title, textKey, method, onSave) {
 	var processedText = method();
 	if ($tw.node && cachingEnabled(wiki)) {
 		saveTiddlerCache(wiki, title, processedText, onSave);
+	} else {
+		if (onSave) {
+			onSave(null, false, processedText);
+		}
 	}
 	return processedText;
 };
+
+var path;
+if ($tw.node) {
+	path = require('path');
+}
 
 function saveTiddlerCache(wiki, title, text, onSave) {
 	var newTiddler = new $tw.Tiddler({title: title, text: text});
@@ -32,7 +45,9 @@ function saveTiddlerCache(wiki, title, text, onSave) {
 	onSave = onSave || function(err) {
 		console.log("Cached:", title, err);
 	};
-	$tw.utils.saveTiddlerToFile(newTiddler, fileInfo, onSave);
+	$tw.utils.saveTiddlerToFile(newTiddler, fileInfo, function(err) {
+		onSave(err, true, text);
+	});
 };
 
 function cachingEnabled(wiki) {
@@ -44,8 +59,8 @@ function cachingDir(wiki) {
 };
 
 function loadTiddlerCache(wiki, title) {
-	var path = generateCacheFilepath(wiki, title);
-	var info = $tw.loadTiddlersFromFile(path);
+	var filepath = generateCacheFilepath(wiki, title);
+	var info = $tw.loadTiddlersFromFile(filepath);
 	if (info.tiddlers.length > 0) {
 		var tiddler = info.tiddlers[0];
 		return tiddler;
@@ -53,25 +68,26 @@ function loadTiddlerCache(wiki, title) {
 	return undefined;
 };
 
+// TODO: This isn't really tested very much
 function generateCacheFilepath(wiki, title) {
 	// Remove any forward or backward slashes so we don't create directories
-	var filepath = title.replace(/\/|\\/g,"_");
+	var filename = title.replace(/\/|\\/g,"_");
 	// Remove any characters that can't be used in cross-platform filenames
-	filepath = $tw.utils.transliterate(filepath.replace(/<|>|~|\:|\"|\||\?|\*|\^/g,"_"));
+	filename = $tw.utils.transliterate(filename.replace(/<|>|~|\:|\"|\||\?|\*|\^/g,"_"));
 	// Truncate the filename if it is too long
-	if(filepath.length > 200) {
-		filepath = filepath.substr(0,200);
+	if(filename.length > 200) {
+		filename = filename.substr(0,200);
 	}
 	// If the resulting filename is blank (eg because the title is just punctuation characters)
-	if(!filepath) {
+	if(!filename) {
 		// ...then just use the character codes of the title
-		filepath = "";
+		filename = "";
 		$tw.utils.each(title.split(""),function(char) {
-			if(filepath) {
-				filepath += "-";
+			if(filename) {
+				filename += "-";
 			}
-			filepath += char.charCodeAt(0).toString();
+			filename += char.charCodeAt(0).toString();
 		});
 	}
-	return path.resolve(cachingDir(wiki), filepath + '.tid');
+	return path.resolve(cachingDir(wiki), filename + '.tid');
 };
--- tiddlers/test/cache.js
+++ tiddlers/test/cache.js
@@ -8,21 +8,20 @@ Tests the file caching mechanism.
 \*/
 
 var library = require('$:/plugins/flibbles/uglify/cache.js');
-var cacheTiddler = '$:/config/flibbles/ugly/cache';
+var cacheTiddler = '$:/config/flibbles/uglify/cache';
 var dirTiddler = '$:/config/flibbles/uglify/cacheDirectory';
 
 function cache(wiki, title, textKey, method, onsave) {
 	return library.getFileCacheForTiddler(wiki, title, textKey, method, onsave);
 };
 
-function cacheAsync(wiki, title, textKey, method) {
+function cache(wiki, title, textKey, method) {
 	return new Promise((resolve, reject) => {
-		return library.getFileCacheForTiddler(wiki, title, textKey, method, function(err) {
+		return library.getFileCacheForTiddler(wiki, title, textKey, method, function(err, saved, cachedResults) {
 			if (err) {
 				reject(err);
 			} else {
-				// Nothing to return. Maybe I want to change this??
-				resolve();
+				resolve({output: cachedResults, saved: saved});
 			}
 		});
 	});
@@ -45,20 +44,34 @@ if ($tw.node) {
 		// defaults to .cache
 		var name = newName();
 		var path = './.cache/'+name+'.tid';
-		await cacheAsync(wiki, name, 'textContent', () => 'output');
+		var info = await cache(wiki, name, 'textContent', () => 'output');
+		expect(info.saved).toBe(true);
 		const stats = await fs.stat(path);
 		await fs.rm(path);
 		expect(stats).toBeTruthy();
 	});
 
-	//TODO: can caching be disabled? Not with what I have cacheTiddler set to right now.
+	it('caching can be disabled', async function(done) {
+		var wiki = new $tw.Wiki();
+		wiki.addTiddler({title: cacheTiddler, text: 'no'});
+		var name = newName();
+		var path = './.cache/'+name+'.tid';
+		var info = await cache(wiki, name, 'textContent', () => 'output');
+		expect(info.saved).toBe(false);
+		try {
+			await fs.stat(path);
+			done(new Error(`File ${path} was written but caching is disabled`));
+		} catch (err) {
+			done();
+		}
+	});
 
 	it('overwrites existing caches, not make iterative ones', async function() {
 		var wiki = new $tw.Wiki();
 		wiki.addTiddler({title: dirTiddler, text: testDir});
 		var name = newName();
-		await cacheAsync(wiki, name, 'first', () => 'output1');
-		await cacheAsync(wiki, name, 'second', () => 'output2');
+		await cache(wiki, name, 'first', () => 'output1');
+		await cache(wiki, name, 'second', () => 'output2');
 		const files = await fs.readdir(testDir);
 		var nameCount = 0;
 		$tw.utils.each(files, function(file) {
@@ -75,14 +88,14 @@ if ($tw.node) {
 
 } else { // !$tw.node
 
-	it('doesn\'t try caching on the browser', function() {
+	it('doesn\'t try caching on the browser', async function() {
 		var wiki = new $tw.Wiki();
 		// Set this to 'yes', even though it should be overridden.
 		wiki.addTiddler({title: cacheTiddler, text: 'yes'});
 		function onSave(err) {
 			throw new Error('onSave should never be called on the browser since caching is impossible there.');
 		};
-		var output = cache(wiki, 'title', 'textKey', () => 'expected', onSave);
+		var output = await cache(wiki, 'title', 'textKey', () => 'expected', onSave);
 		expect(output).toBe('expected');
 	});
 }
