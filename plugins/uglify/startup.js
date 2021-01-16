471b89672a8669d9f21f1a766c11298cb73c12bd
Cameron Fischer <fischer.cameron@gmail.com>
2021-01-16 00:15:15 -0500

    NodeJS only precaches for servers

--- plugins/uglify/startup.js
+++ plugins/uglify/startup.js
@@ -3,11 +3,12 @@ title: $:/plugins/flibbles/uglify/startup
 module-type: startup
 type: application/javascript
 
-This startup task gets uglifying out of the way immediately on NodeJS so it
-doesn't wait until the first request. If compression is initially disabled,
-nothing happens, and if it's later enabled, it does get done on the next
-request.
+If starting a server, this task gets uglifying out of the way immediately
+on NodeJS so it doesn't wait until the first request. If compression is
+initially disabled, nothing happens, and if it's later enabled, it does
+get done on the next request.
 
+If we're not running a server, don't bother precaching.
 \*/
 
 /*jslint node: true, browser: true */
@@ -17,20 +18,23 @@ request.
 
 exports.name = 'uglify';
 exports.synchronous = true;
-exports.after = ['startup', 'plugins'];
 exports.platforms = ['node'];
 
 var systemTargets = ["$:/boot/boot.js", "$:/boot/bootprefix.js"];
 
 exports.startup = function() {
+	$tw.hooks.addHook('th-server-command-post-start', precache);
+};
+
+function precache() {
 	if ($tw.wiki.compressionEnabled()) {
 		var indexer = $tw.wiki.getIndexer('FieldIndexer');
 		var titles = indexer.lookup('plugin-type', 'plugin');
-		$tw.utils.each(titles, precache);
-		$tw.utils.each(systemTargets, precache);
+		$tw.utils.each(titles, precacheTiddler);
+		$tw.utils.each(systemTargets, precacheTiddler);
 	}
 };
 
-function precache(title) {
+function precacheTiddler(title) {
 	$tw.wiki.getTiddlerCompressedText(title);
 }
