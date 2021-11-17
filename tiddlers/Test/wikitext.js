69b2bbacb24a42eee8f4d9e353b19f736f614fde
Cameron Fischer <fischer.cameron@gmail.com>
2021-11-17 11:16:34 -0500

    moved whitespace to its own suite

--- tiddlers/Test/wikitext.js
+++ tiddlers/Test/wikitext.js
@@ -13,25 +13,6 @@ const wikitextType = "text/vnd.tiddlywiki";
 
 var test = $tw.utils.test.wikitext.test;
 
-it('purges carriage returns when it can', function() {
-	test('A\r\nB', 'A\nB');
-	test('A\r\n\r\nB', 'A\n\nB');
-});
-
-it('handles whitespace trimming', function() {
-	test("\\whitespace trim\n\n\n''Content''\n\n\n", "\n\n''Content''");
-	test("\\whitespace trim\n<div>\n\t''Text''\n</div>", "<div>''Text''");
-	test("\\whitespace trim\n<div>\n\nText\n</div>", "<div>\n\nText");
-	// placeholders in text cannot be trusted
-	test("\\define m(v)\n\\whitespace trim\n<div>\n\t<span>$v$</span>\n</div>\n\\end\n<<m [[''fancy'' content]]>>",
-		"\\define m(v)\n\\whitespace trim\n<div><span>$v$\n\\end\n<<m [[''fancy'' content]]>>");
-	test("\\define m(v)\n\\whitespace trim\n<div>\n\n\t<span>\n\n$v$\n</span></div>\n\\end\n<<m [[* ''fancy'' content]]>>",
-		"\\define m(v)\n\\whitespace trim\n<div>\n\n<span>\n\n$v$\n\n\\end\n<<m [[* ''fancy'' content]]>>");
-	// multiple whitespace pragma
-	test("\\whitespace trim\n\\whitespace notrim\n<div>\n\tText\n</div>", "<div>\n\tText\n");
-	test("\\whitespace notrim\n\\whitespace trim\n<div>\n\tText\n</div>", "<div>Text");
-});
-
 /*
 it('does an identity transform right now', function() {
 	var tested = 0;
---
+++ tiddlers/Test/wikitext/whitespace.js
@@ -0,0 +1,39 @@
+/*\
+title: Test/wikitext/whitespace.js
+type: application/javascript
+tags: $:/tags/test-spec
+
+Tests the wikitext uglifier with whitespace and whitespace trimming.
+
+\*/
+
+describe('wikitext uglifier', function() {
+describe('whitespace', function() {
+
+const test = $tw.utils.test.wikitext.test;
+
+it('purges carriage returns when it can', function() {
+	test('A\r\nB', 'A\nB');
+	test('A\r\n\r\nB', 'A\n\nB');
+});
+
+it('trims', function() {
+	test("\\whitespace trim\n\n\n''Content''\n\n\n", "\n\n''Content''");
+	test("\\whitespace trim\n<div>\n\t''Text''\n</div>", "<div>''Text''");
+	test("\\whitespace trim\n<div>\n\nText\n</div>", "<div>\n\nText");
+});
+
+it('cannot be trimmed for sure around placeholders', function() {
+	// notice how the \whitespace trim isn't going away?
+	test("\\define m(v)\n\\whitespace trim\n<div>\n\t<span>$v$</span>\n</div>\n\\end\n<<m [[''fancy'' content]]>>",
+		"\\define m(v)\n\\whitespace trim\n<div><span>$v$\n\\end\n<<m [[''fancy'' content]]>>");
+	test("\\define m(v)\n\\whitespace trim\n<div>\n\n\t<span>\n\n$v$\n</span></div>\n\\end\n<<m [[* ''fancy'' content]]>>",
+		"\\define m(v)\n\\whitespace trim\n<div>\n\n<span>\n\n$v$\n\n\\end\n<<m [[* ''fancy'' content]]>>");
+});
+
+it('multiple pragma', function() {
+	test("\\whitespace trim\n\\whitespace notrim\n<div>\n\tText\n</div>", "<div>\n\tText\n");
+	test("\\whitespace notrim\n\\whitespace trim\n<div>\n\tText\n</div>", "<div>Text");
+});
+
+});});
