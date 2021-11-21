44bfbf7d65f22fb3b07a28b5bfb61298c90aaa36
Cameron Fischer <fischer.cameron@gmail.com>
2021-11-20 21:12:25 -0500

    Added documentation about trimming whitespace

--- tiddlers/Macros/uglify.js
+++ tiddlers/Macros/uglify.js
@@ -12,17 +12,25 @@ This is used to help demonstrate uglified code of various other tiddlers.
 exports.name = 'uglify';
 
 exports.params = [
-	{name: 'tiddler'}
+	{name: 'tiddler'},
+	{name: 'text'}
 ];
 
-exports.run = function(title) {
+exports.run = function(title, text) {
+	var wiki = this.wiki;
+	if (text) {
+		return uglify(text, wiki);
+	}
 	if (!title) {
 		title = this.getVariable("currentTiddler");
 	}
-	var wiki = this.wiki;
-	var tiddler = this.wiki.getTiddler(title);
+	var tiddler = wiki.getTiddler(title);
 	if (!tiddler) {
 		return '';
 	}
 	return wiki.getTiddlerUglifiedText(title);
+};
+
+function uglify(text, wiki) {
+	return wiki.getUglifier("text/vnd.tiddlywiki").uglify(text, 'test', {wiki: wiki});
 };
