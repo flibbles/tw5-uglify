17dcbffc6872d59fb9bac985ac81b199466f8fec
Cameron Fischer <fischer.cameron@gmail.com>
2023-12-24 13:10:45 -0500

    Javascript sourcemapping is preserved

--- plugins/uglify/javascript/javascript.js
+++ plugins/uglify/javascript/javascript.js
@@ -10,11 +10,114 @@ exports.uglify = function(text, options) {
 	code.text = text;
 	var options = {
 		toplevel: !utils.isSystemTarget(title), // top level can be minified. These are modules.
+		module: false,//!utils.isSystemTarget(title),
 		sourceMap: {},
 		output: {quote_style: 1}}; // single quotes. Smaller in TW.
 	var results = uglifyjs.minify(code, options);
 	if (results.error) {
 		throw results.error;
 	}
+	stripFunctionWrap(results);
 	return {text: results.code, map: results.map};
+};
+
+var fluffWrapper = "!function(){";
+
+function stripFunctionWrap(results) {
+	if (containsFunctionWrap(results.code)) {
+		var map = JSON.parse(results.map);
+		var newMappings = removeFirstTwoEntries(map.mappings);
+		if (newMappings !== null) {
+			results.code = results.code.slice(fluffWrapper.length, -4);
+			map.mappings = newMappings;
+			results.map = JSON.stringify(map);
+		}
+	}
+};
+
+// Snags and translates the first three entries, and creates a single entry
+// to replace them that points directly to the third.
+function removeFirstTwoEntries(mapping) {
+	var ptrs = [],
+		newPtr = [],
+		comma = 0;
+	// Get the 3rd comma
+	while (ptrs.length < 3) {
+		var next = mapping.indexOf(",", comma);
+		if (next < 0) {
+			// We're not finding the three entries like expected.
+			return null;
+		}
+		ptrs.push(toNum(mapping.substring(comma, next)));
+		comma = next+1;
+	}
+	if (ptrs[0][0] !== 0 || ptrs[1][0] !== 1) {
+		// First two should always be: AA..,CA..,
+		// If not, then it's not what we expected. Abort.
+		return null;
+	}
+	// What was at position x should now be at position x-wrapper, or likely 0.
+	newPtr[0] = ptrs[0][0] + ptrs[1][0] + ptrs[2][0] - fluffWrapper.length;
+	// Sum the source, row, and column of all three to get absolute locations
+	newPtr[1] = ptrs[0][1] + ptrs[1][1] + ptrs[2][1];
+	newPtr[2] = ptrs[0][2] + ptrs[1][2] + ptrs[2][2];
+	newPtr[3] = ptrs[0][3] + ptrs[1][3] + ptrs[2][3];
+	if (ptrs[2][4] !== undefined) {
+		newPtr[4] = (ptrs[0][4] || 0) + (ptrs[1][4] || 0) + ptrs[2][4];
+	}
+	// We also want to remove the last two mappings.
+	var lastComma = mapping.lastIndexOf(",");
+	lastComma = mapping.lastIndexOf(",", lastComma-1);
+	return toQLT(newPtr) + mapping.substring(comma-1,lastComma);
+};
+
+function containsFunctionWrap(code) {
+	return (code.indexOf(fluffWrapper) === 0
+		&& code.lastIndexOf("}();") === code.length-4);
+};
+
+const h64map = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+function toQLT(array) {
+	var string = "";
+	for (var i = 0; i < array.length; i++) {
+		var number = array[i];
+		var bits = 0;
+		if (number < 0) {
+			bits = 1;
+			number = -number;
+		}
+		bits += (number * 2) & 30;
+		number >>= 4;
+		while (number > 0) {
+			string += h64map[bits + 32];
+			bits = number & 31;
+			number >>= 5;
+		}
+		string += h64map[bits];
+	}
+	return string;
+};
+
+function toNum(letters) {
+	var array = [];
+	for (var i = 0; i < letters.length; i++) {
+		var number = h64map.indexOf(letters[i]);
+		var sign = number & 1;
+		var continuation = !!(number & 32)
+		number = (number & 30) >> 1;
+		var significants = 4
+		while (i < letters.length && continuation) {
+			i++;
+			var next = h64map.indexOf(letters[i]);
+			number += (next & 31) << significants;
+			significants += 5;
+			continuation = !!(next & 32)
+		}
+		if (sign) {
+			number = -number;
+		}
+		array.push(number);
+	}
+	return array;
 };
--- tiddlers/Test/javascript.js
+++ tiddlers/Test/javascript.js
@@ -12,7 +12,7 @@ describe('javascript uglifier', function() {
 
 function compress(input, title) {
 	title = title || 'test';
-	return $tw.wiki.getUglifier('application/javascript').uglify(input, {title: title}).text;
+	return $tw.wiki.getUglifier('application/javascript').uglify(input, {title: title});
 };
 
 function exec(text) {
@@ -24,13 +24,13 @@ function exec(text) {
 };
 
 it('modules', function() {
-	var text = compress("exports.add = function(first, second) {return first + second; }");
+	var text = compress("exports.add = function(first, second) {return first + second; }").text;
 	var exports = exec(text);
 	expect(exports.add(4,6)).toBe(10);
 });
 
 it('prefers single quotes', function() {
-	var text = compress("exports.singles = 'singles';\nexports.doubles = \"doubles\";");
+	var text = compress("exports.singles = 'singles';\nexports.doubles = \"doubles\";").text;
 	expect(text).not.toContain('"');
 	const exports = exec(text);
 	expect(exports.singles).toBe('singles');
@@ -38,7 +38,7 @@ it('prefers single quotes', function() {
 });
 
 it('minifies variables at top level scope', function() {
-	const text = compress('var counterVar=0;\nexports.count = function() { return counterVar++; }');
+	const text = compress('var counterVar=0;\nexports.count = function() { return counterVar++; }').text;
 	expect(text).not.toContain('counterVar');
 	const exports = exec(text);
 	expect(exports.count()).toBe(0);
@@ -46,21 +46,61 @@ it('minifies variables at top level scope', function() {
 	expect(exports.count()).toBe(2);
 });
 
-function sourceMap(input, title) {
-	title = title || 'test';
-	return $tw.wiki.getUglifier('application/javascript').uglify(input, title).map;
-};
-
 it('generates source map', function() {
-	var text = sourceMap("exports.add = function(first, second) {return first + second; }", "directory/test.js");
-	expect(true).toBe(true);
+	var pretty = "exports.add = function(first, second) {return first + second; }";
+	var text = compress(pretty, "directory/test.js").map;
+	// The first mapping should point to the "exports".
+	expect(text).toContain("\"AAAAA,");
 });
 
 it('can handle backticks?', function() {
-	var text = compress('exports.run = function(status) { return `backticks are ${status}`; }');
+	var text = compress('exports.run = function(status) { return `backticks are ${status}`; }').text;
 	expect(text).not.toContain("status");
 	var exports = exec(text);
 	expect(exports.run('working')).toBe('backticks are working');
+});
+
+function removeWrapping(text, expectedNewMapping, log) {
+	var results = compress(text);
+	var ugly = results.text
+	expect(ugly).toContain('function');
+	expect(ugly).toContain('use strict');
+	// Function shouldn't show up twice.
+	expect(ugly.substr(ugly.indexOf('function')+8)).not.toContain('function');
+	expect(results.map).toContain('"' + expectedNewMapping + ',');
+	if (log) {
+		console.log("###");
+		console.log(text);
+		console.log("---");
+		console.log(ugly);
+		console.log("---");
+		console.log(results.map);
+		console.log("###");
+	}
+	//*/
+	results.exports = exec(ugly);
+	return results;
+};
+
+it('can remove wrapping functions', function() {
+	var pretty = '/**Function stuff\n */\n(function() {\n"use strict";\nexports.identity = function() {};\n                  })();';
+	expect(removeWrapping(pretty, "AAGA", true).exports.identity()).toBe(undefined);
+	// Test with some indentation
+	pretty = '/**Function stuff\n */\n       (     function() {\n   "use strict";\nexports.factorial = function(number) { return number * (number + 1) / 2; }\n})();';
+	expect(removeWrapping(pretty, "AAGG").exports.factorial(5)).toBe(15);
+	// Test with some crazy indentation
+	pretty = '/**Function stuff\n */\n'+' '.repeat(645)+'(function() {\n\t"use strict";\nexports.factorial = function(number) { return number * (number + 1) / 2; }\n})();';
+	expect(removeWrapping(pretty, "AAGC").exports.factorial(5)).toBe(15);
+	// Test with a lot of newlines
+	pretty = '/**Function stuff'+'\n'.repeat(530)+'*/\n(function() {\n"use strict";\nexports.factorial = function(number) { return number * (number + 1) / 2; }\n})();';
+	expect(removeWrapping(pretty, "AAohBA").exports.factorial(5)).toBe(15);
+});
+
+it('can remove wrapping from nothing', function() {
+	var pretty = '(function() {\n"use strict";\n})();';
+	var results = compress(pretty);
+	expect(results.text).toBe('');
+	expect(results.map).toContain('"mappings":""');
 });
 
 });
