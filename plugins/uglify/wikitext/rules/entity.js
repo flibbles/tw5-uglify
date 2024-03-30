c3713e3e57ae875e0b810cb6a3822ea51b4c7c49
Cameron Fischer <fischer.cameron@gmail.com>
2024-03-30 15:58:07 -0400

    Fixed issue with &#32; at line starts

--- plugins/uglify/wikitext/rules/entity.js
+++ plugins/uglify/wikitext/rules/entity.js
@@ -9,9 +9,12 @@ var utils = require("../utils.js");
 exports.name = "entity";
 
 exports.uglify = function() {
+	var startsLine = this.parser.pos === 0 || this.parser.source[this.parser.pos-1] === "\n";
 	var entity = this.parse()[0].entity;
 	if (entity === "&#32;") {
-		return [{text: " ", textWithTrim: "&#32;"}];
+		if (!startsLine) {
+			return [{text: " ", textWithTrim: "&#32;"}];
+		}
 	}
 	return entity;
 };
--- tiddlers/Test/wikitext/entity.js
+++ tiddlers/Test/wikitext/entity.js
@@ -8,7 +8,7 @@ Tests the wikitext uglifier with entities.
 \*/
 
 describe('wikitext uglifier', function() {
-describe('comment', function() {
+describe('entity', function() {
 
 const test = $tw.utils.test.wikitext.test;
 const cmp = $tw.utils.test.wikitext.cmp;
@@ -22,6 +22,9 @@ it('supports any entity', function() {
 it('compresses spaces', function() {
 	test(  "I&#32;//love//&#32;bbq", "I //love// bbq");
 	test(t+"I&#32;//love//&#32;bbq", "I //love// bbq");
+});
+
+it('prevents decoding when whitespace pragma must remain', function() {
 	test(  "I&#32;//love//&#32;bbq?test?t?a //b// c?test?",
 	       "I //love// bbq?test?t?a //b// c?test?");
 	test(t+"I&#32;//love//&#32; bbq?test?t?a //b// c?test?",
@@ -30,11 +33,21 @@ it('compresses spaces', function() {
 	       "<div>I //love//  bbq</div>\n?test?t?a //b// c?test?");
 	test(t+"<div>I&#32;//love//&#32; bbq</div>\n?test?t?a //b// c?test?",
 	     t+"<div>I&#32;//love//&#32;bbq</div>?test?t?a //b// c?test?");
+});
+
+it('prevents decoding around macro placeholders', function() {
 	// Placeholders too
 	test("\\define M(m)\nI&#32;//love//&#32; bbq $m$\n\\end\n<<M  'a b'>>",
 	     "\\define M(m)I //love//  bbq $m$\n<<M'a b'>>");
 	test("\\define M(m)\n"+t+"I&#32;//love//&#32; bbq $m$\n\\end\n<<M  'a b'>>",
 	     "\\define M(m)\n"+t+"I&#32;//love//&#32; bbq $m$\n\\end\n<<M'a b'>>");
+});
+
+it('entity at start of block', function() {
+	test(  "&#32;<$reveal />", "&#32;<$reveal/>");
+	test(t+"&#32;<$reveal />", "&#32;<$reveal/>");
+	test(  "C\n\n&#32;<$reveal />", "C\n\n&#32;<$reveal/>");
+	test(t+"C\n\n&#32;<$reveal />", "C\n\n&#32;<$reveal/>");
 });
 
 });});
